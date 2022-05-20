b1098ee5c1f1f76805e5f5d27cccc42085df3868
Rohan <rohgyas@hotmail.com>
2022-05-20 21:51:53 +0400

    Update paging loading state

--- app/src/main/java/com/rrg/sicpa_test/ui/article_list/ArticleListFragment.kt
+++ app/src/main/java/com/rrg/sicpa_test/ui/article_list/ArticleListFragment.kt
@@ -6,6 +6,7 @@ import androidx.core.view.isVisible
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.rrg.sicpa_test.R
@@ -14,10 +15,12 @@ import com.rrg.sicpa_test.databinding.FragmentArticleListBinding
 import com.rrg.sicpa_test.ui.shared.LoadingFooterAdapter
 import com.rrg.sicpa_test.utils.DateTimeFormatter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class ArticleListFragment: Fragment(R.layout.fragment_article_list) {
+class ArticleListFragment : Fragment(R.layout.fragment_article_list) {
 
     @Inject
     lateinit var dateTimeFormatter: DateTimeFormatter
@@ -36,40 +39,42 @@ class ArticleListFragment: Fragment(R.layout.fragment_article_list) {
 
         binding.apply {
             recyclerView.layoutManager = LinearLayoutManager(requireContext())
-            recyclerView.adapter = adapter.withLoadStateFooter(LoadingFooterAdapter { adapter.refresh() })
+            recyclerView.adapter =
+                adapter.withLoadStateFooter(LoadingFooterAdapter { adapter.refresh() })
             recyclerView.setHasFixedSize(true)
             recyclerView.itemAnimator = null
 
-            viewModel.pagedArticles.observe(viewLifecycleOwner){
-                adapter.submitData(viewLifecycleOwner.lifecycle,it)
+            viewModel.pagedArticles.observe(viewLifecycleOwner) {
+                adapter.submitData(viewLifecycleOwner.lifecycle, it)
             }
 
             searchLayout.textField.editText?.doAfterTextChanged {
                 val input = it.toString()
-                if(input.length >= 3 || input.isEmpty()){
+                if (input.length >= 3 || input.isEmpty()) {
                     viewModel.updateSearchQuery(input)
                     recyclerView.scrollToPosition(0)
                 }
             }
 
-            dataLoadingPlaceholderView.onRetry = {adapter.retry()}
+            dataLoadingPlaceholderView.onRetry = { adapter.retry() }
             // Need to map some paging 3 states to the project's states.
-            adapter.addLoadStateListener { loadState ->
+            lifecycleScope.launch {
+                adapter.loadStateFlow.collectLatest { loadState ->
 
-                dataLoadingPlaceholderView.isVisible = (loadState.refresh is LoadState.Loading).also {
-                    dataLoadingPlaceholderView.bind(State.Loading)
-                }
-                recyclerView.isVisible = loadState.source.refresh is LoadState.NotLoading
-                dataLoadingPlaceholderView.isVisible = (loadState.refresh is LoadState.Error).also {
-                    dataLoadingPlaceholderView.bind(State.LoadingFailed(null))
-                }
+                    pagingProgressBar.root.isVisible = loadState.refresh is LoadState.Loading
+                    recyclerView.isVisible = loadState.source.refresh is LoadState.NotLoading
+                    dataLoadingPlaceholderView.isVisible = (loadState.refresh is LoadState.Error).also {
+                            dataLoadingPlaceholderView.bind(State.LoadingFailed(null))
+                        }
 
-                if (loadState.source.refresh is LoadState.NotLoading && loadState.append.endOfPaginationReached && adapter.itemCount < 1) {
-                    recyclerView.isVisible = false
-                    noNewsFoundLayout.root.isVisible = true
-                } else {
-                    recyclerView.isVisible = true
-                    noNewsFoundLayout.root.isVisible = false
+                    if (loadState.source.refresh is LoadState.NotLoading &&
+                        loadState.append.endOfPaginationReached && adapter.itemCount < 1) {
+                        recyclerView.isVisible = false
+                        noNewsFoundLayout.root.isVisible = true
+                    } else {
+                        recyclerView.isVisible = true
+                        noNewsFoundLayout.root.isVisible = false
+                    }
                 }
             }
 
